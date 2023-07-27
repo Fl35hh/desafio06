7c5be11e14109bb3645718001bfdee3c898a0cbc
martingonzalez35 <martingontrabajo@gmail.com>
2023-07-27 19:20:19 -0300

    Ejercicio B

---
+++ ejercicio b.js
@@ -0,0 +1,7 @@
+var cantidadDeGatos = 5 ;
+var cantidadDePasos = 3;
+
+for(var contador = 1; contador <= cantidadDeGatos;  contador++){
+    console.log("Gato #" + contador + ": 🐈" + "🐾".repeat(cantidadDePasos))
+}
+
